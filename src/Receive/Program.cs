412465cc654d636f1fc4deedba25adcafd200692
荣波 <842549829@qq.com>
2019-04-23 16:24:25 +0800

    添加项目文件。

---
+++ src/Receive/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    /*
+     * 详细讲解 https://www.sojson.com/blog/48.html
+     * 基本使用 https://www.cnblogs.com/yan7/p/9498685.html  
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            IConnectionFactory connectionFactory = new ConnectionFactory
+            {
+                HostName = "127.0.0.1",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    //交换机名称
+                    string exchangeName = "exchange3";
+                    //声明交换机
+                    channel.ExchangeDeclare(exchange: exchangeName, type: "topic");
+                    //消息队列名称
+                    int random = new Random().Next(1, 1000);
+                    string queueName = exchangeName + "_" + random.ToString();
+                    //声明队列
+                    channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    //匹配多个路由
+                    channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: "routeKey.*");
+                    //声明为手动确认
+                    channel.BasicQos(0, 1, false);
+                    var consumer = new EventingBasicConsumer(channel);
+                    //接收事件
+                    consumer.Received += (model, ea) =>
+                    {
+                        byte[] message = ea.Body;//接收到的消息
+                        Console.WriteLine("接收到信息为:" + Encoding.UTF8.GetString(message));
+                        //返回消息确认
+                        channel.BasicAck(ea.DeliveryTag, true);
+                    };
+                    //开启监听
+                    channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ src/Send/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start");
+            IConnectionFactory connectionFactory = new ConnectionFactory
+            {
+                HostName = "127.0.0.1",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    //交换机名称
+                    string exchangeName = "exchange3";
+                    //路由名称 
+                    string routeKey = "routeKey.A";  //详细解释 https://www.sojson.com/blog/48.html
+                    //声明交换机   路由交换机类型direct
+                    channel.ExchangeDeclare(exchange: exchangeName, type: "topic");
+                    while (true)
+                    {
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;
+                        properties.MessageId = Guid.NewGuid().ToString("N");
+                        properties.Timestamp = new AmqpTimestamp(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+                        //properties.Headers = new Dictionary<string, object>();
+
+                        Console.WriteLine("消息内容：");
+                        string message = Console.ReadLine();
+                        //消息内容
+                        byte[] body = Encoding.UTF8.GetBytes(message);
+                        //发送消息  发送到路由匹配的消息队列中
+                        channel.BasicPublish(exchange: exchangeName, routingKey: routeKey, basicProperties: null, body: body);
+                        Console.WriteLine("成功发送消息:" + message);
+                    }
+                }
+            }
+        }
+    }
+}
